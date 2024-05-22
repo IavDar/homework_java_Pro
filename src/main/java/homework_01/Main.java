055af08420bfd2311b9168b5ddd6a28dbda135f9
IavDar <elcandess@gmail.com>
2024-05-22 23:12:34 +0200

    homework 01

--- src/main/java/homework_01/Main.java
+++ src/main/java/homework_01/Main.java
@@ -1,8 +1,21 @@
 package homework_01;
 
 public class Main {
+    public static int counter =0;
     public static void main(String[] args) {
 
-        // здесь решение дз
+            for (int i = 1; i <= 2_000_000; i++) {
+
+                if (i % 21 == 0) {
+                    String s = Integer.toString(i);
+                    if (s.contains("3"))
+                    {
+                        counter ++;
+
+                    }
+                }
+        }
+        System.out.println("Количество чисел (один поток): " + counter);
+
     }
 }
--- src/main/java/homework_01/MyClass.java
+++ src/main/java/homework_01/MyClass.java
@@ -1,9 +1,35 @@
 package homework_01;
 
 public class MyClass {
-    public static void main(String[] args) {
-        // дз
-
-        //здесь доработки к дз
+    private static int counter =0;
+    public static synchronized void increment() {
+        counter ++;
     }
+    public static void main(String[] args) {
+
+        MyThread myThread1 = new MyThread();
+        myThread1.start();
+
+
+        for (int i = 1; i <= 1_000_000; i++) {
+
+            if (i % 21 == 0) {
+                String s = Integer.toString(i);
+                if (s.contains("3"))
+                {
+                    increment();
+
+                }
+            }
+        }
+        try {
+            myThread1.join();
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Количество чисел (два потока): " + counter);
+
+    }
+
 }
